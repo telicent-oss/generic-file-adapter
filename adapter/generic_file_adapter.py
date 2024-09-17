b5e4c2577f37c371069070770fcc41da62e38151
Andreas Cola <andreas.cola@telicent.io>
2024-09-17 14:32:07 +0100

    feat: baseline adapter created

---
+++ adapter/generic_file_adapter.py
@@ -0,0 +1,80 @@
+from collections.abc import Iterable
+from logging import Logger, StreamHandler, getLogger
+
+from dotenv import load_dotenv
+from telicent_lib import Adapter, Record
+from telicent_lib.config import Configurator
+from telicent_lib.logging import CoreLoggerFactory
+from telicent_lib.records import RecordUtils
+from telicent_lib.sinks import KafkaSink
+
+load_dotenv()
+# Mapper Configuration
+config = Configurator()
+target_topic= config.get("TARGET_TOPIC", required=True,
+                    description="Specifies the Kafka topic the mapper pushes its output to")
+name = config.get("PRODUCER_NAME", required=True,
+                    description="Specifies the name of the producer")
+source_name=config.get("SOURCE_NAME", required=True,
+                    description="Specifies the source that the data has originated from")
+content_type=config.get("CONTENT_TYPE", required=True,
+                    description="Specifies the content type of the payload source that the data has originated from")
+debug = config.get("DEBUG", required=False, default=False, converter=bool, required_type=bool)
+
+default_security_label = "*"
+
+logger = getLogger(__name__)
+
+def create_records(data, source_filename) -> Iterable[Record]:
+    record = Record(
+        RecordUtils.to_headers(
+            {
+                "Security-Label": default_security_label,
+                "Content-Type": content_type,
+                "Data-Source": source_filename,
+                "Data-Producer": name,
+            }
+        ),
+        None,
+        data.encode('utf-8')
+    )
+    logger.debug(record)
+    return record
+
+def process_source(adapter) -> Record | list[Record] | None:
+    logger.info(f"loading {file_path}")
+    adapter.send(create_records(data, file_path))
+    logger.info(f"File {file_path} Uploaded")
+    return None
+
+if __name__ == "__main__":
+
+    from adapter.data.config import data_files
+
+    logger = CoreLoggerFactory.get_logger(__name__)
+
+    if isinstance(logger, Logger):
+        logger.addHandler(StreamHandler())
+    else:
+        logger.logger.addHandler(StreamHandler())
+
+    # Instantiate Sink for different topics
+    sink_ttl = KafkaSink(target_topic)
+    adapter = Adapter(
+        target=sink_ttl,
+        name=name,
+        source_name=source_name)
+    adapter.run()
+    logger.info("Adapter Created")
+
+    try:
+        for file_path in data_files:
+            # Load ttl file
+            with open('adapter/data/' + file_path) as f:
+                data = f.read()
+            process_source(adapter)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+    finally:
+        logger.info("Adapter finished")
+        adapter.finished()
